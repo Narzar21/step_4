c8f64ae2712be3fddc25a9395bd8d0a4eed5af5d
Narzar21 <exp.two@yandex.ru>
2019-06-17 02:48:19 +0300

    load_project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Table} from 'antd';
 import './App.css';
+import 'antd/dist/antd.css';
+
+export var data=[
+  {
+    key: '0',
+    book: 'book #0',
+    author: 'author_0',
+    year: '1800-1854',
+  },
+  {
+    key: '1',
+    book: 'book #1',
+    author: 'author_1',
+    year: '1803-1885',
+  },
+  {
+    key: '2',
+    book: 'book #2',
+    author: 'author_2',
+    year: '1780-1822',
+  },
+  {
+    key: '3',
+    book: 'book #3',
+    author: 'author_3',
+    year: '1811-1857',
+  },
+  {
+    key: '4',
+    book: 'book #4',
+    author: 'author_4',
+    year: '1866-1904',
+  }
+];
+const col=[
+  {
+    title: 'Наименование книги',
+    dataIndex: 'book',
+  },
+  {
+    title: 'Автор(ы)',
+    dataIndex: 'author',
+    render: text => <a href={text}>{text}</a>,
+  },
+  {
+    title: 'Годы жизни',
+    dataIndex: 'year',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table dataSource={data} columns={col} />
   );
 }
-
+//Sconsole.log(data.length);
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,100 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App, {data} from './App';
+import auth_0 from './img/1.jpg';
+import auth_1 from './img/2.jpg';
+import auth_2 from './img/3.jpg';
+import auth_3 from './img/4.jpg';
+import auth_4 from './img/5.jpg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Main extends React.Component {
+  render() {
+    return(
+    <App />);
+  }
+}
+class Author1 extends React.Component{
+  render(){
+    return(
+      <div align="center">
+        <img src={auth_0} alt={data[0].author} />
+        <br />
+        <p>
+          {data[0].year}
+        </p>
+        <a href="http:\\localhost:3000"><button>Назад</button></a>
+    </div>);
+    }
+}
+class Author2 extends React.Component{
+  render(){
+    return(
+      <div align="center">
+        <img src={auth_1}  alt={data[1].author}/>
+        <br />
+        <p>
+          {data[1].year}
+        </p>
+        <a href="http:\\localhost:3000"><button>Назад</button></a>
+      </div>);
+    }
+}
+class Author3 extends React.Component{
+  render(){
+    return(
+      <div align="center">
+        <img src={auth_2}  alt={data[2].author}/>
+        <br />
+        <p>
+          {data[2].year}
+        </p>
+        <a href="http:\\localhost:3000"><button>Назад</button></a>
+      </div>);
+    }
+}
+class Author4 extends React.Component{
+  render(){
+    return(
+      <div align="center">
+        <img src={auth_3}  alt={data[3].author}/>
+        <br />
+        <p>
+          {data[3].year}
+        </p>
+        <a href="http:\\localhost:3000"><button>Назад</button></a>
+      </div>);
+    }
+}
+class Author5 extends React.Component{
+  render(){
+    return(
+      <div align="center">
+        <img src={auth_4} alt={data[4].author} />
+        <br />
+        <p>
+          {data[4].year}
+        </p>
+        <a href="http:\\localhost:3000"><button>Назад</button></a>
+      </div>);
+    }
+}
+class NotFound extends React.Component{
+    render(){
+        return (<div align="center"><p>Ресурс не найден</p><a href="http:\\localhost:3000"><button>Назад</button></a></div>);
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+<Router>
+  <Switch>
+    <Route exact path="/" component={Main}/>
+    <Route path="/author_0" component={Author1}/>
+    <Route path="/author_1" component={Author2}/>
+    <Route path="/author_2" component={Author3}/>
+    <Route path="/author_3" component={Author4}/>
+    <Route path="/author_4" component={Author5}/>
+    <Route component={NotFound}/>
+  </Switch>
+</Router>
+, document.getElementById('root'));
